f66a7a058297daf1742d17d2006c49ba06beaf77
Yogendra Chand <76557778+Yogendrachand007@users.noreply.github.com>
2021-10-19 16:27:57 +0530

    Add files via upload

---
+++ Practical 7/task8.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+ {
+    int i, j;
+    int n;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+    	for(j=n;j>=i;j--)
+    	{
+    	  cout<<"*";
+    	}
+    	
+    	cout<<endl;
+    }
+   
+   return 0;
+  }
