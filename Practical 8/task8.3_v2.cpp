2742dfd8e29673e6baa0339be541bb6b0375f503
Yogendra Chand <76557778+Yogendrachand007@users.noreply.github.com>
2021-10-22 22:34:40 +0530

    Add files via upload

---
+++ Practical 8/task8.3_v2.cpp
@@ -0,0 +1,16 @@
+//Change the value of a using q pointer to pointer. 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    int *p=&a;
+    int **q=&p;
+    cout<<"a:"<<a<<" ";
+    *p=20;
+    cout<<"p:"<<*p<<" ";
+    **q=30;
+    cout<<"q:"<<**q<<" ";
+    *p=**q;
+    cout<<"p:"<<*p<<endl;
+}
